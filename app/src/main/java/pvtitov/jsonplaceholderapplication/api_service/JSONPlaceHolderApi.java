161c87dcddbe6488e76df3ea858663b84907fe5c
Pavel Titov <pavel_titov@mail.ru>
2017-10-07 16:35:38 +0300

    Started HTTP API service

---
+++ app/src/main/java/pvtitov/jsonplaceholderapplication/api_service/JSONPlaceHolderApi.java
@@ -0,0 +1,27 @@
+package pvtitov.jsonplaceholderapplication.api_service;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+/**
+ * Created by Павел on 07.10.2017.
+ */
+
+public interface JSONPlaceHolderApi {
+    @GET("/posts/{number}")
+    Call<PostsModel> getPost(@Path("number") int number);
+
+    @GET("/comments/{number}")
+    Call<CommentsModel> getComment(@Path("number") int number);
+
+    @GET("/users/{number}")
+    Call<UsersModel> getUser(@Path("number") int number);
+
+    @GET("/photos/{number}")
+    Call<PhotosModel> getPhoto(@Path("number") int number);
+
+    @GET("/todos/{number}")
+    Call<TodosModel> getTodo(@Path("number") int number);
+}
